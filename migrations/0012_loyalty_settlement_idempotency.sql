14089823eda415cc021db3b51bc490c78443096f
CODESPACE <jerome.businessplatform@gmail.com>
2026-09-01 14:57:48 +0800

    Remove duplicate 0012 migration after renumbering to 0015

--- migrations/0012_loyalty_settlement_idempotency.sql
+++
@@ -1,6 +0,0 @@
--- PRIME Sprint 3: make paid-order loyalty settlement idempotent at the database layer.
-PRAGMA foreign_keys = ON;
-
-CREATE UNIQUE INDEX IF NOT EXISTS uq_loyalty_order_payment_earn
-  ON loyalty_transactions(reference_type, reference_id, kind)
-  WHERE reference_type = 'order_payment' AND kind = 'earn';
